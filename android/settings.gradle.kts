f4ff8bbea4cd1a754a44ea464854befedc30de77
timmayboi7 <timmayboi7@gmail.com>
2025-11-24 08:21:01 -0600

    Add palette controls and undo to Compose preview

---
+++ android/settings.gradle.kts
@@ -0,0 +1,20 @@
+import org.gradle.api.initialization.resolve.RepositoriesMode
+
+rootProject.name = "ColorWithFriends"
+include(":app")
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
